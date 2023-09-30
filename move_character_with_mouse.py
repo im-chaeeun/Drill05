49cf7a2fa26eb770a378bfcc4f8bb29d23f17ce0
im-chaeeun <ce0118@tukorea.ac.kr>
2023-09-30 16:45:49 +0900

    캐릭터 이동 완료
    겹쳐서 이동해서 이동 장면 수정 필요

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -1,3 +1,5 @@
+import random
+
 from pico2d import *
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
@@ -5,7 +7,20 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
+mouse = load_image('hand_arrow.png')
 
+def character_move_to_mouse():
+    global mouse_x, mouse_y, x, y, x1, x2
+
+    if mouse_x == x and mouse_y == y:
+        mouse_x, mouse_y = random.randint(0, 1000), random.randint(0, 1000)
+    else:
+        x1, y1 = x, y
+        for i in range(0, 100+1, 4):
+            t = i/100
+            x = (1-t) * x1 + t * mouse_x
+            y = (1-t) * y1 + t * mouse_y
+            character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
 
 def handle_events():
     global running
@@ -24,20 +39,27 @@ def handle_events():
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+mouse_x, mouse_y = random.randint(0, 1000), random.randint(0, 1000)
+x1, y1 = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
+character_delay = 0.05  # 캐릭터의 딜레이
+mouse_delay = 2.0  # 마우스 위치 변경 딜레이
+character_timer = 0.0  # 캐릭터 딜레이 타이머 초기화
+mouse_timer = 0.0  # 마우스 딜레이 타이머 초기화
 hide_cursor()
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+
+    mouse.clip_draw(0, 0, 50, 52, mouse_x, mouse_y)
+    character_move_to_mouse()
     update_canvas()
     frame = (frame + 1) % 8
-
     handle_events()
+    delay(0.3)
 
 close_canvas()
-
 
 
 
